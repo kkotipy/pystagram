0688b7f10d6523e8e254b2624a4aacd7f7b65412
Youngha Park <kkotipy@gmail.com>
2024-10-02 14:43:34 +0900

    Feature: 회원가입
    1. 기본 구조 생성
    2. SignupForm을 사용한 Template 구성
    3. View에 회원가입 로직 구현

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from users.forms import LoginForm
+from users.forms import LoginForm, SignupForm
+from users.models import User
 
 
 def login_view(request):
@@ -34,3 +35,45 @@ def logout_view(request):
     logout(request)
 
     return redirect("/users/login/")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password1 = form.cleaned_data["password1"]
+            password2 = form.cleaned_data["password2"]
+            profile_image = form.cleaned_data["profile_image"]
+            short_description = form.cleaned_data["short_description"]
+
+            # 비밀번호와 비밀번호 확인의 값이 같은지 검사
+            if password1 != password2:
+                form.add_error(
+                    "password2", "비밀번호와 비밀번호 확인란의 값이 다릅니다"
+                )
+
+            # username을 사용 중인 User가 이미 있는지 검사
+            if User.objects.filter(username=username).exists():
+                form.add_error("username", "입력한 사용자명은 이미 사용중입니다")
+
+            # 에러가 존재한다면, 에러를 포함한 form을 사용해 회원가입 페이지를 다시 렌더링
+            if form.errors:
+                context = {"form": form}
+                return render(request, "users/signup.html", context)
+            # 에러가 없다면, 사용자를 생성하고 로그인 처리 후 피드 페이지로 이동
+            else:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password1,
+                    profile_image=profile_image,
+                    short_description=short_description,
+                )
+                login(request, user)
+                return redirect("/posts/feeds/")
+
+    # GET 요청에서는 빈 Form을 보여준다
+    else:
+        form = SignupForm()
+        context = {"form": form}
+        return render(request, "users/signup.html", context)
