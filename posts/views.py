d746d7223012359b7a32e1d8c5fc0653d6d7645e
Youngha Park <kkotipy@gmail.com>
2024-10-03 15:26:07 +0900

    Feature: 댓글 작성
    2. View에서 Template으로 Form 전달
    3. 댓글 작성 처리를 위한 View 구현

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+from posts.forms import CommentForm
 from posts.models import Post
 
 
@@ -7,5 +10,14 @@ def feeds(request):
         return redirect("/users/login/")
 
     posts = Post.objects.all()
-    context = {"posts": posts}
+    comment_form = CommentForm()
+    context = {
+        "posts": posts,
+        "comment_form": comment_form,
+    }
     return render(request, "posts/feeds.html", context)
+
+
+@require_POST
+def comment_add(request):
+    print(request.POST)
