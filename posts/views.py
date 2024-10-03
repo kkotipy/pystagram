46c6763809fe6bad98c55f8f3a246afcc7a894f3
Youngha Park <kkotipy@gmail.com>
2024-10-03 16:01:02 +0900

    Feature: 댓글 작성
    4. form에서 comment_add View로 데이터 전달 및 처리

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 
@@ -20,4 +21,21 @@ def feeds(request):
 
 @require_POST
 def comment_add(request):
-    print(request.POST)
+    # request.POST로 전달된 데이터를 사용해 CommentForm 인스턴스를 생성
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # commit=False 옵션으로 메모리상에 Comment 객체 생성
+        comment = form.save(commit=False)
+
+        # Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
+        comment.user = request.user
+
+        # DB에 Comment 객체 저장
+        comment.save()
+
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+
+        # 생성한 comment에서 연결된 post 정보를 가져와서 id값을 사용
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
