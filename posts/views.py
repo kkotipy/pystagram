295f5918cf516093c31a1b4b5bf753e76dfee5c1
Youngha Park <kkotipy@gmail.com>
2024-10-07 14:08:41 +0900

    Feature: 글 상세 페이지
    1. 기본구조 구현
    2. Template 내용 구현
    3. PostForm 전달

--- posts/views.py
+++ posts/views.py
@@ -116,3 +116,13 @@ def tags(request, tag_name):
     }
 
     return render(request, "posts/tags.html", context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    comment_form = CommentForm()
+    context = {
+        "post": post,
+        "comment_form": comment_form,
+    }
+    return render(request, "posts/post_detail.html", context)
